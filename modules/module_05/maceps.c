fb71fa1431a5330d6ced3f17785b04ef87eaaca4
Joe Koebbe <Joe.Koebbe@usu.edu>
2024-10-02 07:41:49 -0600

    Add files via upload

---
+++ modules/module_05/maceps.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// In the following code, we will discover the precison of number
+// representation on any computer you will use now and in the future
+
+int main()
+{
+  printf("hello");
+}
